cc8c40efd4eb592820b4fd935f2a059c0c37c34e
Ilya McCune-Pedit <ilyamcnpdt@protonmail.com>
2021-06-20 00:14:37 -0400

    add vscode stuff and ablity list vers and server

--- cogs/minecraft.py
+++ cogs/minecraft.py
@@ -1,7 +1,8 @@
+from os import name
 import discord
 from discord.ext import commands
 from discord_slash import cog_ext
-from discord_slash.utils.manage_commands import create_option
+from discord_slash.utils.manage_commands import create_choice, create_option
 
 from modules.getvars import getDiscordVars, getPerm
 import modules.servers as mcservers
@@ -16,25 +17,43 @@ class Minecraft(commands.Cog):
 	def __init__(self, bot):
 			self.bot = bot
 
-	
+	# list minecraft version or servers
 	@cog_ext.cog_slash(
 		guild_ids=guild_ids,
 		name='mclist',
-		description='list avaliable server version/ types')
-	async def mclist(self, ctx):
+		description='list avaliable server version/types',
+		options=[
+			create_option(
+				name='what',
+				description='servers or versions',
+				option_type=3,
+				required=True,
+				choices=[
+					create_choice(
+						name='versions',
+						value='versions'
+					),
+					create_choice(
+						name='servers',
+						value='servers'
+					)
+				]
+			)
+		])
+	async def mclist(self, ctx, what: str):
 		print('executed mclist')
-		await ctx.send(mcservers.listVersion())
+		await ctx.send(mcservers.listVers(what))
 
 
 
-
+	# create a new minecraft server from a supplied version and name
 	@cog_ext.cog_slash(
 		guild_ids=guild_ids,
 		name='mcnew',
 		description='new minecraft server',
 		options=[
 			create_option(
-				name='sname',
+				name='name',
 				description='name for the minecraft server, used in various places for naming and will appear in server list',
 				option_type=3,
 				required=True
@@ -46,12 +65,12 @@ class Minecraft(commands.Cog):
 				required=True
 			),
 		])
-	async def mcnew(self, ctx, sname: str, version: str):
+	async def mcnew(self, ctx, name: str, version: str):
 		print('executed mcnew')
-
+		mcservers.new(name, version)
 		embed=discord.Embed(title="New server", description="new server created", color=0x01bc4f)
-		embed.add_field(name="Name", value=sname, inline=True)
-		embed.add_field(name="Verions", value=version, inline=True)
+		embed.add_field(name="Name", value=name, inline=False)
+		embed.add_field(name="Verion", value=version, inline=False)
 		await ctx.send(embed=embed)
 
 
--- modules/servers.py
+++ modules/servers.py
@@ -3,8 +3,18 @@ import os
 
 from modules.getvars import getPaths
 
-base, templates, server = getPaths()
+base, templates, servers = getPaths()
 
 
-def listVersion():
-	return subprocess.check_output(f'ls {templates} --hide=*installer*'.split(), text=True)
+def listVers(what: str):
+	if what == 'versions':
+		return subprocess.check_output(f'ls {templates} --hide=*installer*'.split(), text=True)
+	else:
+		return subprocess.check_output(f'ls {servers} --hide=*installer*'.split(), text=True)
+
+
+def new(name: str, version: str):
+	os.system(f'cp -R {templates}/{version} {servers}/{name}')
+
+def start(name):
+	os.system(f'')
