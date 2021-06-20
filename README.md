0d8c2820bffa81befdd28f8a49f8d7162062c493
Ilya McCune-Pedit <ilyamcnpdt@protonmail.com>
2021-06-20 16:57:41 -0400

    server starting no work and update readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # pythonbot
+
+rename settings2.json to settings.json set appropriate values
+
+do `pip isntall -U -r requirements.txt` to install python libs
--- modules/servers.py
+++ modules/servers.py
@@ -8,15 +8,18 @@ base, templates, servers = getPaths()
 
 def list(what):
 	if what == 'versions':
-		return subprocess.check_output(f'ls {templates} --hide=*installer*'.split(), text=True)
+		out = subprocess.check_output(f'ls {templates} --hide=*installer*'.split(), text=True)
 	else:
-		return subprocess.check_output(f'ls {servers} --hide=*installer*'.split(), text=True)
+		out =  subprocess.check_output(f'ls {servers} --hide=*installer*'.split(), text=True)
+	if out == '':
+		out = 'no servers or versoin where found'
+	return out
 
 
 def new(name, version):
 	os.system(f'cp -R {templates}{version} {servers}{name}')
 
 def start(name):
-	os.system(f'screen -dmS mc-{name} bash -c {servers}{name}/start')
+	os.system(f'bash -c {servers}{name}/start')
 	# os.system(f'screen -dmS mc-{name}')
 	# os.system(f'screen -X bash {servers}/{name}/start -S mc-{name}')
